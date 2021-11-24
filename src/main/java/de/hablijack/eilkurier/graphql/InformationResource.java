a597b2601c6424a0eeb15332fdcc86569c798257
hablijack <christoph.habel@posteo.de>
2021-11-24 11:47:00 +0100

    Native image xmx set

--- src/main/java/de/hablijack/eilkurier/graphql/InformationResource.java
+++ src/main/java/de/hablijack/eilkurier/graphql/InformationResource.java
@@ -4,11 +4,18 @@ import de.hablijack.eilkurier.entity.Information;
 import java.util.List;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
 
 //@RolesAllowed("user")
 @GraphQLApi
 public class InformationResource {
+
+  @Query("information")
+  @Description("Get one Information stored in database")
+  public Information getInformation(int id) {
+    return Information.findById(id);
+  }
 
   @Query("information")
   @Description("Get all Information stored in database")
@@ -20,5 +27,18 @@ public class InformationResource {
   @Description("Get count of all Information stored in database")
   public int getCount() {
     return Information.findAll().list().size();
+  }
+
+  @Mutation
+  public Information createInformation(Information info) {
+    info.persist();
+    return info;
+  }
+
+  @Mutation
+  public Information deleteInformation(int id) {
+    Information info = Information.findById(id);
+    info.delete();
+    return info;
   }
 }
