aac3e805d2213e251b3d4b78b87e6e0a2d193986
hablijack <christoph.habel@posteo.de>
2021-11-26 08:29:09 +0100

    Fixed Spotbugs

--- src/main/java/de/hablijack/eilkurier/api/FeedResource.java
+++ src/main/java/de/hablijack/eilkurier/api/FeedResource.java
@@ -1,11 +1,12 @@
 package de.hablijack.eilkurier.api;
 
 import de.hablijack.eilkurier.entity.Feed;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import org.jboss.logging.Logger;
 
@@ -19,8 +20,9 @@ public class FeedResource {
   @Inject
   EventBus eventBus;
 
-  @POST
+  @GET
   @Path("feeds/import")
+  @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
   public void startImport() {
     LOGGER.info("Starting to fetch Feeds...");
     List<Feed> allFeeds = Feed.listAll();
