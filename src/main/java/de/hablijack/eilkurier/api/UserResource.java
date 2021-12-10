bcd9fe07627fe225e29964b2cbb430cdf86b99ee
hablijack <christoph.habel@posteo.de>
2021-12-10 12:48:13 +0100

    Add Transaction to user creation

--- src/main/java/de/hablijack/eilkurier/api/UserResource.java
+++ src/main/java/de/hablijack/eilkurier/api/UserResource.java
@@ -4,6 +4,7 @@ import de.hablijack.eilkurier.entity.User;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import org.jboss.logging.Logger;
@@ -18,6 +19,7 @@ public class UserResource {
   @POST
   @Path("users")
   @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
+  @Transactional
   public User createIfNotExists(User user) {
     Optional<User> userSearch = User.findByEmailOptional(user.email);
     if (userSearch.isEmpty()) {
