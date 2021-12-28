5ea26134f4f662363537893fffb9ccfd5aa3e322
hablijack <christoph.habel@posteo.de>
2021-12-28 15:45:00 +0100

    Category API added

---
+++ src/main/java/de/hablijack/eilkurier/api/CategoryResource.java
@@ -0,0 +1,31 @@
+package de.hablijack.eilkurier.api;
+
+import de.hablijack.eilkurier.entity.Category;
+import de.hablijack.eilkurier.entity.Feed;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import org.jboss.logging.Logger;
+
+//@RolesAllowed("user")
+@Path("api")
+@ApplicationScoped
+public class CategoryResource {
+
+  private static final Logger LOGGER = Logger.getLogger(CategoryResource.class.getName());
+
+  @Inject
+  EventBus eventBus;
+
+  @GET
+  @Path("categories")
+  @SuppressFBWarnings(value = "", justification = "Security is another Epic and on TODO")
+  public List<Feed> getAllCategories() {
+    LOGGER.info("Starting to fetch Categories...");
+    return Category.listAll();
+  }
+}
