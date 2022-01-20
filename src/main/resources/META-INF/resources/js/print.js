e53591647a63bd54af119d6344676fabb2188db5
hablijack <christoph.habel@posteo.de>
2022-01-20 11:41:49 +0100

    styling and refactoring newspaper layout

--- src/main/resources/META-INF/resources/js/print.js
+++ src/main/resources/META-INF/resources/js/print.js
@@ -2,7 +2,7 @@ var iso = new Isotope('.collumns', {
     itemSelector: '.collumn',
     percentPosition: true,
     masonry: {
-        // use outer width of grid-sizer for columnWidth
-        columnWidth: '.collumn-small'
+        columnWidth: '.collumn-sizer',
+        gutter: 20
     }
 });
